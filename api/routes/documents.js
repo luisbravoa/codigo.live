4c79c3b090c78963d51fc9117a4b2879aac56b92
luis bravo <info@luisbravoa.com>
2016-09-18 02:18:33 +0100

    QR

--- api/routes/documents.js
+++ api/routes/documents.js
@@ -1,13 +1,14 @@
 var qr = require('qr-image');
 const config = require('../config');
 module.exports = {
-    
+
     qr: function (req, res) {
         var id = req.params.id;
         var code = qr.image(config.baseUrl + id, {
-            type: 'svg'
+            type: 'png',
+            size: 7
         });
-        res.type('svg');
+        res.type('png');
         code.pipe(res);
     }
 
